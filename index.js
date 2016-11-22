57c47ff842d7540d62fb8e968d3090806bda06e6
Andreslt <andreslastrat@gmail.com>
2016-11-21 19:23:21 -0500

    Fixing stuffs

--- index.js
+++ index.js
@@ -29,44 +29,6 @@ app.get('/', (req, res)=>{
     res.send('Bienvenido al sistema CRUD de este Centro Educativo.'); 
 });
 
-/*// Usuarios
-app.get('/login', (req, res)=>{
-    res.sendFile(path.join(__dirname+'/index.html'))
-});
-
-app.get('/signUp', (req, res)=>{
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    var form = new formidable.IncomingForm();
-    form.parse(req, function(err, fields, files) {
-    var cloudinaryPath = fields.name+fields.lastname;
-    console.log(files);
-    var newAlumn = new Alumn ({
-            name: fields.name,
-            lastname: fields.lastname,
-            email: fields.email,
-            password: fields.password,
-            confirmpassword:  fields.confirmpass,
-            image: cloudinaryPath,
-            class_id: fields.class_id,
-            mac:    fields.mac,         
-        }); 
-        if (fields.password === fields.confirmpassword){
-            newAlumn.save((err, result)=>{
-                if(err) console.log(err);
-                else {
-                    imageUp.uploadFile(files.image.path, cloudinaryPath);
-                    console.log(result);
-                    res.json(newAlumn);
-                }
-            });  
-        }else{
-            res.send('Verifique las contraseñas');
-        }             
-    });
-app.post('/signUp', (req, res)=>{
-
-});
-*/
 // Campus
 app.get('/api/centros', (req, res)=>{
     //Listar centros.
@@ -89,7 +51,8 @@ app.post('/api/newCentro', middleware.ensureAuthenticated, (req, res)=>{
     var newcampus = new Campus ({name: req.body.name});
     newcampus.save((err, result)=>{
         if(err) console.log(err);
-        else {console.log(result);
+        else{console.log(result);
+        console.log ('Centro creado');
         res.sendStatus(200);
         }
     });
@@ -100,7 +63,7 @@ app.delete('/api/deleteCentro/:id', middleware.ensureAuthenticated, (req, res)=>
     Campus.findOneAndRemove({_id: req.params.id}, (err, campus)=>{
         if (err) console.log(err);
         console.log(campus);
-        Class.remove({campus: campus.id}, (err)=>{
+        Class.remove({campus_id: campus.id}, (err)=>{
             if (err) console.log(err);
             console.log ('Centro elimando');
             res.sendStatus(200);            
@@ -127,7 +90,7 @@ app.get('/api/Clases', (req, res)=>{
 });
 
 app.get('/api/Clases/:id', (req, res)=>{
-    //Listar clases. id: identificación del centro
+    //Listar clases. id: identificación del clase
     Class.findOne({_id:req.params.id},(err, classe)=>{
         if(err) console.log(err);
         res.json(classe);
@@ -135,20 +98,22 @@ app.get('/api/Clases/:id', (req, res)=>{
 });
 
 app.post('/api/newClase', middleware.ensureAuthenticated, (req, res)=>{
-    //Crear clase.
-    Campus.findOne({name: req.body.centro}, (err, campus)=>{
+    //Crear clase. Al crear la nueva clase, se debe verificar que pertenezca a un Centro válido
+    console.log('campus_id: '+req.body.campus_id);
+    Campus.findOne({_id: req.body.campus_id}, (err, campus)=>{
+        console.log('campus: '+campus);
         if (err) console.log(err);
         if (campus !== null){
         
         var newclase = new Class ({
             name: req.body.name,
             campus_id: campus.id
-        });
-        res.send(newclase);      
+        });   
         newclase.save((err, result)=>{
             if(err) console.log(err);
-            else {console.log(result);
-            res.redirect('/api/clases');
+            else {
+                console.log ('Clase creada');
+                res.sendStatus(200);
             }
         });
         }else{
@@ -161,17 +126,17 @@ app.delete('/api/deleteClase/:id', middleware.ensureAuthenticated, (req, res)=>{
     //Eliminar clase. id: identificación de clase
     Class.remove({_id: req.params.id}, (err, result)=>{
         if (err) console.log(err);
-        console.log ('Centro elimando');
-        res.redirect('/api/Centros');
+        console.log ('Clase elimanda');
+        res.sendStatus(200);
     });    
 });
 
 app.put('/api/updateClase/:id', middleware.ensureAuthenticated, (req, res)=>{
     //Actualizar clase. id: identificación de clase
-    Class.update({_id: req.params.id}, (err, result)=>{
+    Class.update({_id: req.params.id}, {$set:{name: req.body.newName}},(err, result)=>{
         if (err) console.log(err);
-        console.log ('Centro elimando');
-        res.redirect('/api/Centros');
+        console.log ('Clase actualizada');
+        res.sendStatus(200);
     });    
 });
 
@@ -234,16 +199,16 @@ app.delete('/api/deleteAlumno/:id', middleware.ensureAuthenticated, (req, res)=>
     Alumn.remove({_id: req.params.id}, (err, result)=>{
         if (err) console.log(err);
         console.log ('Alumno elimando');
-        res.redirect('/api/Centros');
+        res.sendStatus(200);
     });    
 });
 
 app.put('/api/updateAlumno/:id', middleware.ensureAuthenticated, (req, res)=>{
     // Actualizar alumno. id: identificación de alumno
-    Alumn.update({_id: req.params.id}, {$set: req.body}, (err, result)=>{
+    Alumn.update({_id: req.params.id}, {$set: {name: req.body.name}}, (err, result)=>{
         if (err) console.log(err);
         console.log ('Alumno actualizado');
-        res.redirect('/api/Centros');
+        res.sendStatus(200);
     });    
 });
 
